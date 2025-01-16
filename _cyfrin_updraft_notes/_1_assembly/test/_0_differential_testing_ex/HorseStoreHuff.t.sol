2d0de0faef07209060a6dbcf80dc6964cdb8c28a
nathan-websculpt <websculpt.nathan@gmail.com>
2025-01-16 11:46:57 -0600

    differential testing

---
+++ _cyfrin_updraft_notes/_1_assembly/test/_0_differential_testing_ex/HorseStoreHuff.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.20;
+
+import {Base_TestV1} from "./Base_TestV1.t.sol";
+
+contract HorseStoreSolc is Base_TestV1 {
+    function setUp() public override {
+        //huff edition
+        //this way both huff and sol tests can run at once
+        // horseStore = 
+    }
+}
