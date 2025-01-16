50461356519b9def9be659a6265bc137bb7045ea
nathan-websculpt <websculpt.nathan@gmail.com>
2025-01-16 12:03:29 -0600

    test notes

--- _cyfrin_updraft_notes/_1_assembly/test/_0_differential_testing_ex/HorseStoreHuff.t.sol
+++ _cyfrin_updraft_notes/_1_assembly/test/_0_differential_testing_ex/HorseStoreHuff.t.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: GPL-3.0-only
 pragma solidity 0.8.20;
 
-import {Base_TestV1} from "./Base_TestV1.t.sol";
+import {Base_TestV1, HorseStore} from "./Base_TestV1.t.sol";
+import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
+//^^^ you will need the following in your foundry.toml file for this to work:
+//              ffi = true
+//              remappings = ['foundry-huff/=lib/foundry-huff/src']
 
 contract HorseStoreSolc is Base_TestV1 {
     function setUp() public override {
         //huff edition
         //this way both huff and sol tests can run at once
-        // horseStore = 
+
+        string public constant HORSE_STORE_HUFF_LOCATION = "horseStoreV1/HorseStore";
+        //^^^ assumes you are in SRC, and does NOT look for a '.huff'
+        horseStore = HorseStore(HuffDeployer.config().deploy(HORSE_STORE_HUFF_LOCATION));
     }
 }
