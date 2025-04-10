5a79b2f17c8fe4ddb0af3c9fa1e8669e6e0e2851
nathan-websculpt <websculpt.nathan@gmail.com>
2025-04-10 16:54:17 -0500

    update length on pop, tests written for pop

--- arrays/test/Base_Test.t.sol
+++ arrays/test/Base_Test.t.sol
@@ -21,11 +21,46 @@ abstract contract Base_Test is Test {
         assertEq(arraysContract.getValAtIndexOfMyArray(0), val);
     }
 
+    function testPushMultipleItemsToMyArray() public {
+        uint256 val = 2;
+        uint256 val2 = 4;
+        uint256 val3 = 6;
+        arraysContract.pushToMyArray(val);
+        assertEq(arraysContract.getLengthOfMyArray(), 1);
+        assertEq(arraysContract.getValAtIndexOfMyArray(0), val);
+        arraysContract.pushToMyArray(val2);
+        assertEq(arraysContract.getLengthOfMyArray(), 2);
+        assertEq(arraysContract.getValAtIndexOfMyArray(1), val2);
+        arraysContract.pushToMyArray(val3);
+        assertEq(arraysContract.getLengthOfMyArray(), 3);
+        assertEq(arraysContract.getValAtIndexOfMyArray(2), val3);
+    }
+
     function testPopFromMyArray() public {
         uint256 val = 2;
         arraysContract.pushToMyArray(val);
         assertEq(arraysContract.getLengthOfMyArray(), 1);
+        assertEq(arraysContract.getValAtIndexOfMyArray(0), val);
         arraysContract.popFromMyArray();
+        assertEq(arraysContract.getValAtIndexOfMyArray(0), 0);
+        assertEq(arraysContract.getLengthOfMyArray(), 0);
+    }
+
+    function testMultiplePopsFromMyArray() public {
+        uint256 val = 2;
+        uint256 val2 = 4;
+        arraysContract.pushToMyArray(val);
+        assertEq(arraysContract.getLengthOfMyArray(), 1);
+        assertEq(arraysContract.getValAtIndexOfMyArray(0), val);
+        arraysContract.pushToMyArray(val2);
+        assertEq(arraysContract.getLengthOfMyArray(), 2);
+        assertEq(arraysContract.getValAtIndexOfMyArray(1), val2);
+
+        arraysContract.popFromMyArray();
+        assertEq(arraysContract.getValAtIndexOfMyArray(1), 0);
+        assertEq(arraysContract.getLengthOfMyArray(), 1);
+        arraysContract.popFromMyArray();
+        assertEq(arraysContract.getValAtIndexOfMyArray(0), 0);
         assertEq(arraysContract.getLengthOfMyArray(), 0);
     }
 
